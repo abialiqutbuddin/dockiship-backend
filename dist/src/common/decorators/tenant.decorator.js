c048f6f4cff580f46609d9d397b3cb1856a59f33
Abi Ali Qutbuddin <abialigadi@gmail.com>
2025-10-18 16:13:00 +0500

    fix

--- dist/src/common/decorators/tenant.decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TenantId = void 0;
-const common_1 = require("@nestjs/common");
-exports.TenantId = (0, common_1.createParamDecorator)((_, ctx) => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.tenantId ?? req.user?.tenantId;
-});
-//# sourceMappingURL=tenant.decorator.js.map
